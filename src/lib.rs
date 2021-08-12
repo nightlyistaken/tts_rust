4f0a345e614c2204d636e1b9014943140192147b
dhairy-online <dhairysrivastava5@gmail.com>
2021-08-12 16:14:16 +0530

    add docs and up cargo

--- src/lib.rs
+++ src/lib.rs
@@ -2,6 +2,8 @@ use gtts::save_to_file;
 use std::fs;
 use std::io::BufReader;
 use std::path::Path;
+/// This play_mp3 function plays mp3 example: 
+/// play_mp3("./path.mp3");
 pub fn play_mp3(mp3: &str) {
     let (_stream, handle) = rodio::OutputStream::try_default().unwrap();
     let sink = rodio::Sink::try_new(&handle).unwrap();
@@ -10,7 +12,8 @@ pub fn play_mp3(mp3: &str) {
     sink.append(rodio::Decoder::new(BufReader::new(file)).unwrap());
     sink.sleep_until_end();
 }
-
+/// This function takes the text and plays it by speaking the text
+/// Example: text_speech("Hello, Rust!");
 pub fn text_speech(input: &str) {
     save_to_file(input, "audio.mp3");
     play_mp3("audio.mp3");

7f77bbc799a1d8785558d0fc70b2120af5fde176
= <62501544+dhairy-online@users.noreply.github.com>
2021-12-05 22:29:41 +0530

    feat: add CLI

---
+++ cli/src/main.rs
@@ -0,0 +1,23 @@
+use structopt::StructOpt;
+use tts_rust::{ GTTSClient, languages::Languages };
+use quicli::prelude::*;
+
+
+#[derive(Debug, StructOpt)]
+struct GTTSCli {
+    text: String,
+
+    #[structopt(short = "l", long = "lang", default_value = "en", possible_values = &Languages::variants())]
+    language: Languages,
+}
+
+
+fn main() -> CliResult {
+    let args = GTTSCli::from_args();
+    let client = GTTSClient {
+        volume: 1.0,
+        language: args.language,
+    };
+    client.speak(&args.text);
+    Ok(())
+}
